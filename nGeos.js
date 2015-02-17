ffec4dbf660707cdce6a51af9235997c13c3ab31
Kerhann <jferet@gmail.com>
2015-02-17 14:04:13 +0000

    Second commit

--- nGeos.js
+++ nGeos.js
@@ -1,60 +1,51 @@
-Menu = new TAPi18n.Collection("menu");
+
+
+getUserLanguage = function () {
+  // The logic for determining the user language
+var language ="";
+   //first, we check if user is connected and take his/her language preference from his/her profile
+   if (Meteor.user()) {
+     language = "de";
+   }
+   else if (Cookie.get('language') !== undefined)
+      {
+         language = Cookie.get('language');
+      }
+   else if (navigator.language !== null)
+      {
+         language = navigator.language;      
+      }
+   
+return language;
+
+};
 
 
 if (Meteor.isClient) {
     
-    Template.body.helpers({
+   Meteor.startup(function () {
+    Session.set("showLoadingIndicator", true);
 
+    TAPi18n.setLanguage(getUserLanguage())
+      .done(function () {
+        Session.set("showLoadingIndicator", false);
+      })
+      .fail(function (error_message) {
+        // Handle the situation
+        console.log(error_message);
+      });
   });
-    
-    
-    Template.body.helpers({
-    menu: function () {
-      return Menu.find({ isTitle: {$ne: true} }, {sort: {weight: -1}});
-    },
-    menutitle: function () {
-      return Menu.find({ isTitle: true }, {});
-    }
-  });
-
-  Template.menu_title.rendered = function () {
- 
+   
+   
+   
+    Template.landingBody.rendered = function () {
         $.getScript('/js/smoothscroll.js');
-      
-  }
-    
-    
-  Template.menuitem.rendered = function () {
- 
-        $.getScript('/js/smoothscroll.js');
-    
-        
-		// Menu settings
-			$('#menuToggle').on('click', function(){
-                $('#theMenu').css("zIndex", 11);
-                $('.menu-close').css("zIndex", 12);
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
-            $('.menu-close').on('click', function(){
-                $('#theMenu').css("zIndex", 8);
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
-
-    
-    	
-}
-    
-    
-    Template.hello.rendered = function () {
- 
-        $.getScript('/js/retina-1.1.0.js');
-        $.getScript('/js/jquery.cubeportfolio.js');    
+        $.getScript('/js/jquery.superslides.js'); 
+        $.getScript('/js/retina-1.1.0.js');  
         $.getScript('/js/theme.js');
-        $.getScript('/js/smoothscroll.js');
+        $.getScript('/js/jquery.cubeportfolio.min.js');
+ 
+       
     
         //Superslides 
 		$('#slides').superslides({
@@ -65,8 +56,9 @@ if (Meteor.isClient) {
 		    scrollable: true,
 		    animation: 'fade'
 		});
-
-    
+        //$.getScript('/js/map.js');
+       
+   
         // Banner text add parallax efect and fade
         function EasyPeasyParallax() {
 	       scrollPos = $(this).scrollTop();
@@ -81,32 +73,102 @@ if (Meteor.isClient) {
 	   $(document).ready(function () {
 	       $(window).scroll(function () {
                 EasyPeasyParallax();
-	       });
+	       });  
+         $('#grid-container').cubeportfolio({
+        layoutMode: 'slider',
+        drag: true,
+        rewindNav: true,
+        scrollByPage: false,
+        animationType: 'rotateSides',
+        gapHorizontal: 10,
+        gapVertical: 10,
+        gridAdjustment: 'responsive',
+        caption: 'overlayBottomPush',
+        displayType: 'sequentially',
+        displayTypeSpeed: 100,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: true,
+        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+        },
+
+        // singlePageInline
+        singlePageInlineDelegate: '.cbp-singlePageInline',
+        singlePageInlinePosition: 'below',
+        singlePageInlineInFocus: true,
+        singlePageInlineCallback: function(url, element) {
+            // to update singlePageInline content use the following method: this.updateSinglePageInline(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 5000
+                })
+                .done(function(result) {
+
+                    t.updateSinglePageInline(result);
+
+                })
+                .fail(function() {
+                    t.updateSinglePageInline("Error! Please refresh the page!");
+                });
+        }
+    });
 	   });
-    
+
+       
+       
     			// Menu settings
 			$('#menuToggle').on('click', function(){
-                $('#theMenu').css("zIndex", 11);
-                $('.menu-close').css("zIndex", 12);
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
+        
+         // if ($('#theMenu').hasClass('menu-open'))
+         // {
+         // }
+        //else
+         // {
+            $('#theMenu').css("zIndex", 11);
+            $('.menu-close').css("zIndex", 12);
+				    $('#menuToggle').toggleClass('active');
+				    $('body').toggleClass('body-push-toleft');
+				    $('#theMenu').toggleClass('menu-open'); 
+                $('#languages').toggleClass('languages-open');
+             $('#toggleIcon').toggleClass('ion-chevron-left');
+             $('#toggleIcon').toggleClass('ion-chevron-right');
+         // }
 			});
-            $('.menu-close').on('click', function(){
-                $('#theMenu').css("zIndex", 8);
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+      
+        $('.menuitem').on('click', function(){
+            $('#theMenu').css("zIndex", 8);
+            $('.menu-close').css("zIndex", 8);
+				    $('#menuToggle').toggleClass('active');
+				    $('body').toggleClass('body-push-toleft');
+				    $('#theMenu').toggleClass('menu-open'); 
+                $('#languages').toggleClass('languages-push-toleft');
+             $('#toggleIcon').toggleClass('ion-chevron-left');
+             $('#toggleIcon').toggleClass('ion-chevron-right');
+			  });
 
 }
-    
+
 }
 
 if (Meteor.isServer) {
 
   Meteor.startup(function () {
     // code to run on server at startup
-    
-  });
+
+
+});
 }
