02060f45f1fa5295c999b67a688e2bd535524b0f
Kerhann <jferet@gmail.com>
2015-02-19 23:22:48 +0000

    Fourth commit

--- public/js/menu.js
+++ public/js/menu.js
@@ -8,22 +8,24 @@
          // {
             $('#theMenu').css("zIndex", 11);
             $('.menu-close').css("zIndex", 12);
-				    $('#menuToggle').toggleClass('active');
-				    $('body').toggleClass('body-push-toleft');
-				    $('#theMenu').toggleClass('menu-open'); 
-                $('#languages').toggleClass('languages-open');
-             $('#toggleIcon').toggleClass('ion-chevron-left');
-             $('#toggleIcon').toggleClass('ion-chevron-right');
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open'); 
+            $('#logBut').toggleClass('loginButtonDiv-open');
+            $('#languages').toggleClass('languages-open');
+            $('#toggleIcon').toggleClass('ion-chevron-left');
+            $('#toggleIcon').toggleClass('ion-chevron-right');
          // }
 			});
       
         $('.menuitem').on('click', function(){
             $('#theMenu').css("zIndex", 8);
             $('.menu-close').css("zIndex", 8);
-				    $('#menuToggle').toggleClass('active');
-				    $('body').toggleClass('body-push-toleft');
-				    $('#theMenu').toggleClass('menu-open'); 
-                $('#languages').toggleClass('languages-push-toleft');
-             $('#toggleIcon').toggleClass('ion-chevron-left');
-             $('#toggleIcon').toggleClass('ion-chevron-right');
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open'); 
+            $('#logBut').toggleClass('loginButtonDiv-open');
+            $('#languages').toggleClass('languages-open');
+            $('#toggleIcon').toggleClass('ion-chevron-left');
+            $('#toggleIcon').toggleClass('ion-chevron-right');
 			  });
